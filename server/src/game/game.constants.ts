b1f8f1fe126763fb5ec232cddb23cbb888242ea3
dennisb <dennisborsh@gmail.com>
2022-08-20 09:17:32 +0300

    interval fix

--- server/src/game/game.constants.ts
+++ server/src/game/game.constants.ts
@@ -1,6 +1,6 @@
-export const ROWS =  50;
-export const COLS =  50;
-export const GAME_INTERVAL =  1000;
+export const ROWS = 50;
+export const COLS = 50;
+export const GAME_INTERVAL = 500;
 export const CELLS_POSITIONS = [
   [0, 1],
   [0, -1],
