4bc82bc8af92bb4f83d1fd3b9a5484d442123871
dennisb <dennisborsh@gmail.com>
2022-08-23 17:03:26 +0300

    memo added for cell

--- client/src/components/Board/GameCell.tsx
+++ client/src/components/Board/GameCell.tsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import { StyledCell } from './Game.styled';
 import { useUpdateGameCell } from "../Game.api.hooks";
 
-export const GameCell = ({ row, col, value, isGameRunning }: { row: number, col: number, value: boolean, isGameRunning: boolean }) => {
+interface GameCellProps {
+    row: number,
+    col: number,
+    value: boolean,
+    isGameRunning: boolean
+}
 
-  const { mutate } = useUpdateGameCell()
-  const onClick = () => !isGameRunning && mutate({ row, col, value })
-  return (
-    <StyledCell value={value} onClick={onClick} />
-  );
-};
+export const GameCell = React.memo(({ row, col, value, isGameRunning }: GameCellProps) => {
+
+    const { mutate } = useUpdateGameCell()
+    const onClick = () => !isGameRunning && mutate({ row, col, value })
+    return (
+        <StyledCell value={value} onClick={onClick}/>
+    );
+});
 
